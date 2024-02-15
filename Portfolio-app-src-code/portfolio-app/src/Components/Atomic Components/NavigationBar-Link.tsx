a474ee9944e47a6d2ab8d271813c4f24dae66f0d
kaloyanpepelyashki <kaloyan.pepelyashki@gmail.com>
2024-02-15 22:01:30 +0100

    Added the logic for the click to scroll and some styling
    Did some css styling
    Added the logic for the "click to scroll event". The scroll logic is based on the useRef react hook. The logic is triggered by a simple function, that sets the current view to scroll downb to the ScrollTrigger

--- Portfolio-app-src-code/portfolio-app/src/Components/Atomic Components/NavigationBar-Link.tsx
+++ Portfolio-app-src-code/portfolio-app/src/Components/Atomic Components/NavigationBar-Link.tsx
@@ -4,12 +4,13 @@ import { NavLink } from 'react-router-dom';
 interface Props {
     text: string,
     to: string,
+    size?: string
 }
 
- const NavBarLink: React.FC<Props> = ({text, to}) => {
+ const NavBarLink: React.FC<Props> = ({text, to, size}) => {
     return (
         <>
-            <NavLink className="nav-bar-link" to={to}><b className="nav-bar-code-symbol nav-bar-code-symbol-left">&lt;</b>{text} /<b className="nav-bar-code-symbol nav-bar-code-symbol-right">&gt;</b></NavLink>
+            <NavLink className="nav-bar-link" style={{"fontSize": `${size}px`}} to={to}><b className="nav-bar-code-symbol nav-bar-code-symbol-left">&lt;</b>{text} /<b className="nav-bar-code-symbol nav-bar-code-symbol-right">&gt;</b></NavLink>
         </>
     )
 }
--- Portfolio-app-src-code/portfolio-app/src/Pages/HomePage.tsx
+++ Portfolio-app-src-code/portfolio-app/src/Pages/HomePage.tsx
@@ -1,7 +1,43 @@
+import { useRef } from "react";
+import NavBarLink from "../Components/Atomic Components/NavigationBar-Link";
+import { ArrowDropDownCircleOutlined } from '@mui/icons-material'
+
 export default function HomePage() {
+    // const containerRef = useRef<HTMLDivElement | null>(null);
+
+    // const scrollToTarget = (targetId: string) => {
+    //   const container = containerRef.current;
+    //   const targetElement = document.getElementById(targetId);
+    //   if (container && targetElement) {
+    //     // Calculate the position to scroll to
+    //     const containerTop = container.getBoundingClientRect().top;
+    //     const targetTop = targetElement.getBoundingClientRect().top;
+    //     // Since scrollTop is a property of the container, ensure container is typed correctly
+    //     const scrollPosition = targetTop - containerTop + container.scrollTop;
+    //     container.scrollTo({ top: scrollPosition, behavior: 'smooth' });
+    //   }
+    // };
+
+    const scrollTarget = useRef<HTMLDivElement | null>(null)
+
+    const ScrollTrigger = () => scrollTarget.current?.scrollIntoView()   
+        
     return (
-        <div className="home-page-main-wrapper page-main-wrapper">
+        <div  className="home-page-main-wrapper page-main-wrapper">
             <h1 className="home-page-named-heading">I am <b>Kaloyan</b></h1>
+            <div className="linked-headings-holder">
+                <h3 className="home-page-linked-heading">Check out my <NavBarLink text={"work"} to={""} size={"47"} /></h3>
+                <h3 className="home-page-linked-heading">Learn more <NavBarLink text={"about"} to={""} size={"47"} /> me</h3>
+            </div>
+            <div className="home-page-bottom-heading-wrapper">
+                <h4 className="hope-page-bottom-heading">See why I am a perfect fit</h4>
+                <a onClick={(e) => { e.preventDefault(); ScrollTrigger()}}>
+                    <ArrowDropDownCircleOutlined  className="home-page-arrow-down-icon" sx={[{ fontSize: 50, marginTop: "40px", transition: "all", transitionDuration: "0.6s" }, {'&:hover': { transform: "scale(1.2)",}}]}/>
+                </a>
+            </div>
+            <div  id="targetElementId" className="home-page-hidden-section-holder">
+                <h2 ref={scrollTarget}>Heading</h2>
+            </div>
         </div>
     )
 }
