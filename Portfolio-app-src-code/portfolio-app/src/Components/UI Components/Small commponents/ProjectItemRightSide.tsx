47990d05b2db32d0e409f791a8e15305b3844cd3
kaloyanpepelyashki <kaloyan.pepelyashki@gmail.com>
2024-03-07 17:34:45 +0100

    Some minor CSS adjustments on Work page
    Added hover effects over the projects items. Added an arrow next to the project title.

--- Portfolio-app-src-code/portfolio-app/src/Components/UI Components/Small commponents/ProjectItemRightSide.tsx
+++ Portfolio-app-src-code/portfolio-app/src/Components/UI Components/Small commponents/ProjectItemRightSide.tsx
@@ -1,3 +1,5 @@
+import { ArrowOutward } from "@mui/icons-material";
+
 interface ProjectItemRightSide {
   projectTitle: string;
   projectDescription: string;
@@ -10,7 +12,23 @@ const ProjectItemRightSide: React.FC<ProjectItemRightSide> = ({
     <>
       <div className="project-item-top-section-right-side-wrapper">
         <div className="project-right-side-text-holder">
-          <h2 className="project-item-heading">{projectTitle}</h2>
+          <div className="project-right-side-heeading-icon-holder">
+            <h2 className="project-item-heading">{projectTitle}</h2>
+            <span>
+              <ArrowOutward
+                className="project-item-arrow"
+                sx={[
+                  {
+                    fontSize: 30,
+                    transition: "all",
+                    transitionDuration: "0.6s",
+                    marginLeft: "10px",
+                  },
+                  { "&:hover": { transform: "translate(6px, -10px)" } },
+                ]}
+              />
+            </span>
+          </div>
           <p className="project-item-description">{projectDescription}</p>
         </div>
       </div>
--- Portfolio-app-src-code/portfolio-app/src/Pages/WorkPage.tsx
+++ Portfolio-app-src-code/portfolio-app/src/Pages/WorkPage.tsx
@@ -7,20 +7,8 @@ export default function WorkPage() {
   return (
     <>
       <div className="work-page-main-wrapper page-main-wrapper">
-        <h2>Work page</h2>
+        <h2 className="comment-like-heading work-comment-heading">//My work</h2>
         <div className="projects-list-holder">
-          {/* <ProjectItemComponent
-            projectTitle={"Salon Sila"}
-            projectDescription={
-              "Lorem ipsum dolor sit amet, consectetur adipiscing  Ut enim ad minim veniam, quis nostrud exercitation ullamco"
-            }
-            projectImageURL={
-              "https://marketplace.canva.com/EAE6WTyrSQ0/2/0/1600w/canva-light-beige-sleek-and-simple-blogger-personal-website--7Q4-7tyJj4.jpg"
-            }
-            projectRole={"Developer"}
-            projectDate={"05.2022"}
-            projectPurpose={"University Project"}
-          /> */}
           {projects.length > 0
             ? projects.map((project) => {
                 console.log(project);
